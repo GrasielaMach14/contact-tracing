8140a04a5d9519ff6306c921260b578d654c1504
GrasielaMach14 <aciatuani@gmail.com>
2024-01-16 20:14:38 -0300

    CTHealthHeaderController

--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -4,21 +4,14 @@
         // Use a variável scope para pegar os valores tanto de person quanto de location
         const scope = component.get("v.scope");
     createRecordEvent.setParams({
-        //Use o scope para verificar se o que foi chamado oi person ou location
+        //Use o scope para verificar se o que foi chamado foi person ou location
         "entityApiName": scope === "person" ? "Person__c" : "Location__c"
     });
     createRecordEvent.fire();
     },
-
+    // Chame a função help da contagem aqui
     getCountStatus : function(component, event, helper){
-        const scope = component.get("v.scope");
-        const count = component.get("v.count");
-
-        if(scope === "person"){
-            const statusPerson = component.get("c.getPersonHealthStatusCount");
-        }else{
-            const statusLocation = component.get("c.getLocationHealthStatusCount");
-        }
+        helper.fetchStatusCount(component);
 
     }
 })
--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderHelper.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderHelper.js
@@ -1,5 +1,18 @@
 ({
-    helperMethod : function() {
+    // Este método é para fazer a chamada das funções em CTHealthHeaderController
+    fetchStatusCount : function(component) {
+        const scope = component.get("v.scope");
+        // Defina se o escopo for person então chame a função do controller
+        let action = scope === "person" ? component.get("c.getPersonHealthStatusCount") : component.get("c.getLocationHealthStatusCount");
+        //Defina um callback para saber a resposta do sistema, se for exato mude a contagem
+        action.setCallback(this, function (response) {
+            const state = response.getState();
+            if(state === "SUCCESS"){
+                component.set("v.count", response.getReturnValue());
+            }
+            
+        });
 
+        $A.enqueueAction(action);
     }
 })

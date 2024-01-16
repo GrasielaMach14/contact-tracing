0dc9169028eafdfced5fec39b9a24a7c04d6b335
GrasielaMach14 <aciatuani@gmail.com>
2024-01-16 01:09:51 -0300

    Criação CTHealthHeaderController

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
-
+    // Pegar o id para efetuar a mudança no evento
+    handleSelect : function(component, event, helper) {
+        const selectedTabId = event.getParam("id");
+        if(selectedTabId === "person"){
+            component.set("v.title", "Person View");
+        }else{
+            component.set("v.title", "Location View");
+        }
+        // Armazene os atributos do id aqui no scope
+        component.set("v.scope", selectedTabId);
     }
 })
--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -1,5 +1,24 @@
 ({
-    myAction : function(component, event, helper) {
+    createRecord : function(component, event, helper) {
+        const createRecordEvent = $A.get("e.force:createRecord");
+        // Use a variável scope para pegar os valores tanto de person quanto de location
+        const scope = component.get("v.scope");
+    createRecordEvent.setParams({
+        //Use o scope para verificar se o que foi chamado oi person ou location
+        "entityApiName": scope === "person" ? "Person__c" : "Location__c"
+    });
+    createRecordEvent.fire();
+    },
+
+    getCountStatus : function(component, event, helper){
+        const scope = component.get("v.scope");
+        const count = component.get("v.count");
+
+        if(scope === "person"){
+            const statusPerson = component.get("c.getPersonHealthStatusCount");
+        }else{
+            const statusLocation = component.get("c.getLocationHealthStatusCount");
+        }
 
     }
 })

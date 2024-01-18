f88dd9e614255b09a6db898aa670f8abdfeeff82
GrasielaMach14 <aciatuani@gmail.com>
2024-01-18 01:09:43 -0300

    Mudei de novo

--- force-app/main/default/aura/CTRencentChanges/CTRencentChangesController.js
+++ force-app/main/default/aura/CTRencentChanges/CTRencentChangesController.js
@@ -2,16 +2,7 @@
     init: function (component, event, helper) {
         const scope = component.get("v.scope");
 
-        if(scope === "person"){
-            component.set('v.columns', [
-                {label: 'Name', fieldName: 'Name', type: 'text'},
-                {label: 'Name', fieldName: 'Name', type: 'text'},
-                {label: 'Phone', fieldName: 'Mobile__c', type: 'phone'},
-                {label: 'Token', fieldName: 'Token__c', type: 'text'},
-                {label: 'Health Status', fieldName: 'Health_Status__c', type: 'text'},
-                {label: 'Status Update Date', fieldName: 'Status_Update_Date__c', type: 'date'}
-            ]);
-        }else{ 
+        if(scope === "location"){
             component.set('v.columns', [
                 {label: 'Name', fieldName: 'Name', type: 'text'},
                 {label: 'Status', fieldName: 'Status__c', type: 'text'},
@@ -19,6 +10,16 @@
                 {label: 'Address', fieldName: 'Address__c', type: 'text'},
                 {label: 'Red Score', fieldName: 'Red_Score__c', type: 'number'},
                 {label: 'Status Update Date', fieldName: 'Status_Update_Date__c', type: 'date'}
+            ]);
+        }else{ 
+            component.set('v.columns', [
+                {label: 'Name', fieldName: 'Name', type: 'text'},
+                {label: 'Name', fieldName: 'Name', type: 'text'},
+                {label: 'Phone', fieldName: 'Mobile__c', type: 'phone'},
+                {label: 'Token', fieldName: 'Token__c', type: 'text'},
+                {label: 'Health Status', fieldName: 'Health_Status__c', type: 'text'},
+                {label: 'Status Update Date', fieldName: 'Status_Update_Date__c', type: 'date'}
+
             ]);
         }
         helper.fetchData(component);
